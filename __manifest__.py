f448f90cb5a39f7396da6e2ba5b5a77e5a96cf74
Robert N <rbrtmt100@gmail.com>
2020-07-31 20:24:20 +0300

    add folder structure and manufest file

---
+++ __manifest__.py
@@ -0,0 +1,17 @@
+# @ 2020 rbrtmt100@gmail.com
+{
+    'name': "Pass extra variables to the Define Taxes as Python Code module",
+    'summary': "Allows to users the tax python code module to pass in subtotal, and discount variables",
+    'description': """Allows to users the tax python code module to pass in subtotal, and discount variables""",
+    'author': "Robert Nyandoro",
+    'website': "",
+    'category': 'account',
+    'version': '1.0',
+    'license': 'AGPL-3',
+    'depends': [
+        'account_tax_python'
+    ],
+    'data': [
+        'views/account_tax_view.xml'
+    ],
+}
